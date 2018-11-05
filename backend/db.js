4d504fb554ba9226ba52cc84fd3c2b0ec48e6bcf
jingfei <chenjingfei.cn@gmail.com>
2018-11-04 22:40:15 -0800

    modify

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,5 @@
 'use strict';
-// const express    = require('express');
-// const app        = express();   
-// const bodyParser = require('body-parser');
-
-// // configure app to use bodyParser()
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
 const mongoose   = require('mongoose');
-// const url = 'mongodb://admin:c123456@ds117623.mlab.com:17623/userlist';
-
-// mongoose.connect(url, {useNewUrlParser: true}, (err) => {
-//     if (err) throw err;
-//     console.log("Successfully connected to the database");});
-
 const Schema = mongoose.Schema;
 const User  = mongoose.model('User', new Schema({
     firstName: String,
